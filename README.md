cf10d6b0b9aea8db5f1cbaf13727c787e34cc6db
Matt <theprankmonkey@gmail.com>
2019-06-15 21:46:56 -0400

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Event Manager
+
+## Features
+
+- get list of user boardgames from BoardGameGeek account.
+- look for other players with similar game taste.
+- assign players to a free table.
---
+++ classes.py
@@ -0,0 +1,32 @@
+''' Classes for Event Manager '''
+
+class Player(object):
+    def __init__(self, name, username):
+        self.name = name
+        self.username = username
+    def __str__(self):
+        return self.username
+
+
+class Table(object): 
+    def __init__(self, seats):
+        self.seats = seats
+        self.players = []
+    def add_player(self, player):
+        if len(self.players) < self.seats:
+            if player not in self.players:
+                self.players.append(player)
+            else:
+                print("Player {} already at table".format(player.username))
+        else:
+            print("This table is full.")
+            #raise FullTable
+    def remove_player(self, player):
+        if player in self.players:
+            self.players.remove(player)
+        else:
+            print("Player not at table.")
+    def remaining_seats(self):
+        return self.seats - len(self.players)
+    def print_players(self):
+        return [str(x) for x in self.players]
